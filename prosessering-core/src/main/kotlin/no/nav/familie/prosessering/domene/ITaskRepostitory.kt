161e60d882b7c7d3a1b5b945fb356fa9c0a8d044
Johan Blomgren <johan.blomgren@nav.no>
2020-10-09 10:31:30 +0200

    Forslag på generic taskRepository

---
+++ prosessering-core/src/main/kotlin/no/nav/familie/prosessering/domene/ITaskRepostitory.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.prosessering.domene
+
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.PagingAndSortingRepository
+import java.time.LocalDateTime
+
+@NoRepositoryBean
+interface ITaskRepostitory<T> : PagingAndSortingRepository<T, Long> {
+    fun findByStatusInAndTriggerTidBeforeOrderByOpprettetTidDesc(status: List<Status>,
+                                                                 triggerTid: LocalDateTime,
+                                                                 page: Pageable): List<T>
+
+    fun findByStatus(status: Status): List<T>
+
+    fun findByStatusIn(status: List<Status>, page: Pageable): List<T>
+
+    fun findByStatusAndTriggerTidBefore(status: Status, triggerTid: LocalDateTime): List<T>
+}
