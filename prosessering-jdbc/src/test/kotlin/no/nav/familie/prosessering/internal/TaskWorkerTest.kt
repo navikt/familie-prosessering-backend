af09bf079023a18fe1c17401233db0f9d022e1e6
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-10 10:03:40 +0200

    Endret testen

--- prosessering-jdbc/src/test/kotlin/no/nav/familie/prosessering/internal/TaskWorkerTest.kt
+++ prosessering-jdbc/src/test/kotlin/no/nav/familie/prosessering/internal/TaskWorkerTest.kt
@@ -1,15 +1,12 @@
 package no.nav.familie.prosessering.internal
 
 import com.ninjasquad.springmockk.SpykBean
-import io.mockk.every
 import no.nav.familie.prosessering.TestAppConfig
 import no.nav.familie.prosessering.domene.Status
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.familie.prosessering.task.TaskStep1
-import no.nav.familie.prosessering.util.isOptimisticLocking
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -20,7 +17,6 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.transaction.TestTransaction
-import java.util.concurrent.atomic.AtomicBoolean
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [TestAppConfig::class])
@@ -49,34 +45,6 @@ class TaskWorkerTest {
         val findByIdOrNull = repository.findByIdOrNull(savedTask.id)
         assertThat(findByIdOrNull?.status).isEqualTo(Status.FERDIG)
         assertThat(findByIdOrNull?.logg).hasSize(4)
-    }
-
-    @Test
-    internal fun `skal håndtere 2 prosesser som oppdaterer en task`() {
-        val called = AtomicBoolean(false)
-        val task = repository.save(Task(TaskStep1.TASK_1, "{'a'='b'}"))
-        TestTransaction.flagForCommit()
-        TestTransaction.end()
-
-        every { repository.save(any()) } answers {
-            if (called.compareAndSet(false, true)) {
-                Thread.sleep(1000)
-            }
-            callOriginal()
-        }
-        val first = Thread {
-            val exception = catchThrowable { worker.markerPlukket(task.id) } as Exception
-            assertThat(isOptimisticLocking(exception)).isTrue
-        }
-
-        val second = Thread {
-            Thread.sleep(200)
-            worker.markerPlukket(task.id)
-        }
-        first.start()
-        second.start()
-        first.join()
-        second.join()
     }
 
 }
