9146af499abb40ff24bdf0d8120fa7b5a574cfb6
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-28 13:50:16 +0200

    Update prosessering-jdbc/src/test/kotlin/no/nav/familie/prosessering/internal/TaskStepExecutorServiceTest.kt
    Co-authored-by: Henning Håkonsen <henning.hakonsen@nav.no>

--- prosessering-jdbc/src/test/kotlin/no/nav/familie/prosessering/internal/TaskStepExecutorServiceTest.kt
+++ prosessering-jdbc/src/test/kotlin/no/nav/familie/prosessering/internal/TaskStepExecutorServiceTest.kt
@@ -36,7 +36,7 @@ import java.util.UUID
 
 @EnableScheduling
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(classes = [TestAppConfig::class], )
+@ContextConfiguration(classes = [TestAppConfig::class])
 @DataJdbcTest(excludeAutoConfiguration = [TestDatabaseAutoConfiguration::class])
 class TaskStepExecutorServiceTest {
 
