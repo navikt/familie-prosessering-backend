a7c6f649a428cc89ed5c24033520ba0581a86918
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-25 15:06:47 +0100

    Legger til default melding i RekjørSenereException og legger til esception før man går inn i doFeilhåndtering (#199)

--- prosessering-core/src/main/kotlin/no/nav/familie/prosessering/error/RekjørSenereException.kt
+++ prosessering-core/src/main/kotlin/no/nav/familie/prosessering/error/RekjørSenereException.kt
@@ -2,4 +2,5 @@ package no.nav.familie.prosessering.error
 
 import java.time.LocalDateTime
 
-data class RekjørSenereException(val årsak: String, val triggerTid: LocalDateTime): RuntimeException()
+data class RekjørSenereException(val årsak: String, val triggerTid: LocalDateTime)
+    : RuntimeException("Rekjører senere - triggerTid=$triggerTid")
