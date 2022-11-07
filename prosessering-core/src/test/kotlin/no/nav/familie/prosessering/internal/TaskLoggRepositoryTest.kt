b052a4b7bd70716c3cfcf78b5b74b65da5b28ea8
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-07 10:33:36 +0100

    Skal hente siste kommentaren for metadata og ikke feil (#332)

---
+++ prosessering-core/src/test/kotlin/no/nav/familie/prosessering/internal/TaskLoggRepositoryTest.kt
@@ -0,0 +1,31 @@
+package no.nav.familie.prosessering.internal
+
+import no.nav.familie.prosessering.IntegrationRunnerTest
+import no.nav.familie.prosessering.TaskFeil
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskLoggRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+internal class TaskLoggRepositoryTest : IntegrationRunnerTest() {
+
+    @Autowired
+    private lateinit var taskService: TaskService
+
+    @Autowired
+    private lateinit var taskLoggRepository: TaskLoggRepository
+
+    @Test
+    internal fun `skal hente metadata til task`() {
+        var task = taskService.save(Task("minType", "payload"))
+        task = taskService.kommenter(task, "kommentar på task", "", false)
+        task = taskService.feilet(task, TaskFeil(task, RuntimeException("Feil")), 3, 3, false)
+        task = taskService.ferdigstill(task)
+
+        val taskLoggMetadata = taskLoggRepository.finnTaskLoggMetadata(listOf(task.id)).single()
+        assertThat(taskLoggMetadata.taskId).isEqualTo(task.id)
+        assertThat(taskLoggMetadata.antallLogger).isEqualTo(4)
+        assertThat(taskLoggMetadata.sisteKommentar).isEqualTo("kommentar på task")
+    }
+}
