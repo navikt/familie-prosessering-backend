82c70ca17636e0ab6a98f2a1434a8a877c6f1c13
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-12-16 12:21:58 +0100

    Bump dependencies

--- prosessering-core/src/main/kotlin/no/nav/familie/prosessering/rest/RestTaskService.kt
+++ prosessering-core/src/main/kotlin/no/nav/familie/prosessering/rest/RestTaskService.kt
@@ -8,7 +8,6 @@ import no.nav.familie.prosessering.internal.TaskService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
