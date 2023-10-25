cea36a9ced364a7ffc6a0db4d3ed78898b13182c
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-10-25 18:11:55 +0700

    Fikser feil der rekjøring ikke tok hensyn til max antall feil

--- prosessering-core/src/main/kotlin/no/nav/familie/prosessering/error/RekjørSenereException.kt
+++ prosessering-core/src/main/kotlin/no/nav/familie/prosessering/error/RekjørSenereException.kt
@@ -4,3 +4,6 @@ import java.time.LocalDateTime
 
 data class RekjørSenereException(val årsak: String, val triggerTid: LocalDateTime) :
     RuntimeException("Rekjører senere - triggerTid=$triggerTid")
+
+data class MaxAntallRekjøringerException(val maxAntallRekjøring: Int) :
+    RuntimeException("Nådd max antall rekjøring - $maxAntallRekjøring")
